3f5fa19c397ac67335e887c48fc95ab413cb921c
AzizbekAzimjonovich <azizbekazimjonovich@gmail.com>
2024-01-30 18:59:34 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,46 @@
+async function weather(shaxar) {
+    const url = `https://open-weather13.p.rapidapi.com/city/${shaxar}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5729361896msh338361719c968e7p1ae79djsn5bac4e79d7d7',
+            'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function showWeather() {
+    const shaharEl = document.getElementById('hudud');
+    const shaxar = shaharEl.value;
+
+    
+        const block = await weather(shaxar);
+
+        console.log(block);
+
+        const obHavoMaLumotlariDiv = document.getElementById('block');
+
+        if (block) {
+            obHavoMaLumotlariDiv.innerHTML += `
+                <p>${block.name}</p>
+                <p>${block.sys.country}</p>
+                <p>${block.weather[0].description}</p>
+                <p>${block.main.temp}°C</p>
+                <p>${block.main.humidity}% humidity</p>
+                <hr>
+            `;
+        } else {
+            obHavoMaLumotlariDiv.innerHTML = 'xato';
+            
+        }
+    }
+
